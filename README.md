de02dc00db9489716f73ddd0104958771db29a06
Charles Sena <charlessena@gokeitecnologia.com.br>
2025-02-10 13:33:34 +0000

    Feature first implementation

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # youtube-transcriptor
 A youtube video transcriptor script
+
+## Note
+
+Is necessary the [`youtube_transcript_api`](https://pypi.org/project/youtube-transcript-api/) module to be installed.
+Depending on the local python configuration, the use of virtual env it's recommended.
+
+## Use sample
+
+```bash
+echo [video_id] | python ./youtube-transcriptor.py
+```
+
--- youtube-transcriptor.py
+++ youtube-transcriptor.py
@@ -1,3 +1,15 @@
-#!/usr/bin/env python
+#! /usr/bin/env python
 
-from youtube_transcript_api import YoutubeTranscriptApi
+from youtube_transcript_api import YouTubeTranscriptApi
+from sys import stdin
+
+def get_transcript(video_id,languages):
+    transcript = YouTubeTranscriptApi.get_transcript(video_id,languages)
+    text = ' '.join([d['text'] for d in transcript])
+    return text
+
+video_id = stdin.read()
+languages = ["pt"]
+
+print(get_transcript(video_id,languages))
+
